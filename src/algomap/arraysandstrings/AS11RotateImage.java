b7cdd7d08277ef4af7e828818eef4fe9000ab0c8
Sagnik Chakraborty <sagnikchakraborty910@yahoo.com>
2024-10-27 00:43:58 +0530

    algomap: array - RotateImage filename change

--- src/algomap/arraysandstrings/AS11RotateImage.java
+++ src/algomap/arraysandstrings/AS11RotateImage.java
@@ -3,7 +3,7 @@ package algomap.arraysandstrings;
 import java.util.Arrays;
 
 // https://leetcode.com/problems/rotate-image/description/
-public class AS10RotateImage {
+public class AS11RotateImage {
     public static void main(String[] args) {
         int[][] arr = {{1,2,3},{4,5,6},{7,8,9}};
         rotate_approach1(arr);
